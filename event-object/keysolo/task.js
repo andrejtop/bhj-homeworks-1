b3619467720ae1b903877bd54d2e1badbd6a32fa
AndreyToptaev <Bonafide111@yandex.ru>
2023-11-27 15:32:38 +0300

    solo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,12 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keypress', event => {
+        if (this.currentSymbol.textContent.toLowerCase() === event.key.toLowerCase())
+          this.success();
+        else
+          this.fail();
+      })
   }
 
   success() {
